012816858efc57ffb281cf8cdc858736b3de52f2
Jorge Porras <porrasgeorge@gmail.com>
2020-12-28 14:21:16 -0600

    remove if-else station 33

--- Read_Stations_1hr.py
+++ Read_Stations_1hr.py
@@ -5,18 +5,17 @@ stations = db.read_stations() ## All stations to scrap data
 if stations is not None:
     for i, station in stations.iterrows():
 
-        if station["id"] == 33:
-            station_data = ws.IMN_read_station_webpage(station)    ## station data
+        station_data = ws.IMN_read_station_webpage(station)    ## station data
+        if station_data is not None:
+            station_vars = list(station_data.columns)       ## list of all variables in this station
+            db.write_variables(station_vars)                ## check all variables are in database (stored procedure)
+            db.write_data_values(station, station_data)
+        
+        if station["Name"] == "UCR Santa Cruz":
+            
+            station_data = ws.IMN_read_station_webpage(station, 2)    ## station data
             if station_data is not None:
                 station_vars = list(station_data.columns)       ## list of all variables in this station
                 db.write_variables(station_vars)                ## check all variables are in database (stored procedure)
                 db.write_data_values(station, station_data)
             
-            if station["Name"] == "UCR Santa Cruz":
-                
-                station_data = ws.IMN_read_station_webpage(station, 2)    ## station data
-                if station_data is not None:
-                    station_vars = list(station_data.columns)       ## list of all variables in this station
-                    db.write_variables(station_vars)                ## check all variables are in database (stored procedure)
-                    db.write_data_values(station, station_data)
-                
