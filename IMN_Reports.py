bdfbf30ac1bbaea1da11e1eff5eea1a0a838a06f
Jorge Porras <porrasgeorge@gmail.com>
2020-12-28 08:18:55 -0600

    UCR Santa Cruz

--- IMN_Reports.py
+++ IMN_Reports.py
@@ -4,34 +4,66 @@ from datetime import date
 from dateutil.relativedelta import relativedelta
 from pathlib import Path
 
-base_path = f'\\\\192.168.30.30\\Planificacion\\Estaciones_IMN'
-Path(base_path).mkdir(parents=True, exist_ok=True)
-end_date = date.today()+ relativedelta(days=1)
-# initial_date = end_date - relativedelta(months=1)
-initial_date = end_date - relativedelta(days=2)
-date_range_5min = pd.DataFrame({'DateTime': pd.date_range(start=initial_date+relativedelta(minutes=5), end=end_date, freq="5min")})
-date_range_1hr = pd.DataFrame({'DateTime': pd.date_range(start=initial_date+relativedelta(minutes=60), end=end_date, freq="60min")})
+def daily_report():
+    base_path = f'\\\\192.168.30.30\\Planificacion\\Estaciones_IMN'
+    Path(base_path).mkdir(parents=True, exist_ok=True)
+    end_date = date.today() ##+ relativedelta(days=1)
+    # initial_date = end_date - relativedelta(months=1)
+    initial_date = end_date - relativedelta(days=1)
+    date_range_5min = pd.DataFrame({'DateTime': pd.date_range(start=initial_date+relativedelta(minutes=5), end=end_date, freq="5min")})
+    date_range_1hr = pd.DataFrame({'DateTime': pd.date_range(start=initial_date+relativedelta(minutes=60), end=end_date, freq="60min")})
 
-variables = db.read_variables()
-stations = db.read_stations()
-ordered_stations = list(stations["Name"])
-print(ordered_stations)
+    variables = db.read_variables()
+    stations = db.read_stations()
+    ordered_stations = list(stations["Name"])
+    print(ordered_stations)
 
-# Create a Pandas Excel writer using XlsxWriter as the engine.
-writer = pd.ExcelWriter(f'{base_path}\\IMN_{initial_date.strftime("%Y_%m_%d")}.xlsx', engine='xlsxwriter', datetime_format='dd/mm/yyyy hh:mm') # pylint: disable=abstract-class-instantiated
-workbook  = writer.book
+    # Create a Pandas Excel writer using XlsxWriter as the engine.
+    writer = pd.ExcelWriter(f'{base_path}\\IMN_{initial_date.strftime("%Y_%m_%d")}.xlsx', engine='xlsxwriter', datetime_format='dd/mm/yyyy hh:mm') # pylint: disable=abstract-class-instantiated
+    workbook  = writer.book
 
-for _, variable in variables.iterrows():
-    data_df = db.read_data_by_variable(variable["id"], initial_date, end_date)
-    data_spreaded = data_df.pivot(index="DateTime", columns="Station", values="Value")
-    station_in_df = sorted(set(ordered_stations) & set(data_df["Station"]), key = ordered_stations.index)
-    data_spreaded = data_spreaded[station_in_df]
-    if variable["Name"] == "Rio Zapote":
-        data_spreaded = pd.merge(date_range_5min,data_spreaded,on='DateTime',how='left')
-    else:
-        data_spreaded = pd.merge(date_range_1hr,data_spreaded,on='DateTime',how='left')
-    data_spreaded.to_excel(writer, sheet_name=variable["Name"], index=False) #, startrow=4)
-    worksheet = writer.sheets[variable["Name"]]
-    worksheet.set_column('A:A', 18)
+    for _, variable in variables.iterrows():
+        data_df = db.read_data_by_variable(variable["id"], initial_date, end_date)
+        data_spreaded = data_df.pivot(index="DateTime", columns="Station", values="Value")
+        station_in_df = sorted(set(ordered_stations) & set(data_df["Station"]), key = ordered_stations.index)
+        data_spreaded = data_spreaded[station_in_df]
+        if variable["Name"] == "Rio Zapote":
+            data_spreaded = pd.merge(date_range_5min,data_spreaded,on='DateTime',how='left')
+        else:
+            data_spreaded = pd.merge(date_range_1hr,data_spreaded,on='DateTime',how='left')
+        data_spreaded.to_excel(writer, sheet_name=variable["Name"], index=False) #, startrow=4)
+        worksheet = writer.sheets[variable["Name"]]
+        worksheet.set_column('A:A', 18)
 
-writer.save()
+    writer.save()
+
+def monthly_report():
+    base_path = f'\\\\192.168.30.30\\Planificacion\\Estaciones_IMN'
+    Path(base_path).mkdir(parents=True, exist_ok=True)
+    end_date = date.today()
+    initial_date = end_date - relativedelta(months=1)
+    variables = db.read_variables()
+    stations = db.read_stations()
+    ordered_stations = list(stations["Name"])
+    # Create a Pandas Excel writer using XlsxWriter as the engine.
+    # writer = pd.ExcelWriter(f'{base_path}\\IMN_{initial_date.strftime("%Y_%m_%d")}.xlsx', engine='xlsxwriter', datetime_format='dd/mm/yyyy hh:mm') # pylint: disable=abstract-class-instantiated
+    # workbook  = writer.book
+
+    for _, variable in variables.iterrows():
+        data_df = db.read_data_by_variable(variable["id"], initial_date, end_date)
+        data_spreaded = data_df.pivot(index="DateTime", columns="Station", values="Value")
+        #data_spreaded = data_df.pivot(columns="Station", values="Value")
+        station_in_df = sorted(set(ordered_stations) & set(data_df["Station"]), key = ordered_stations.index)
+        data_spreaded = data_spreaded[station_in_df]
+        data_spreaded["group_Date"] = data_spreaded.index - pd.Timedelta(minutes=1)
+        data_spreaded["group_Date"] = pd.to_datetime(data_spreaded["group_Date"]).dt.to_period('D')
+
+        print(data_spreaded)
+        
+        # data_spreaded.to_excel(writer, sheet_name=variable["Name"], index=False) #, startrow=4)
+        # worksheet = writer.sheets[variable["Name"]]
+        # worksheet.set_column('A:A', 18)
+
+    # writer.save()
+
+monthly_report()
--- web_scraping.py
+++ web_scraping.py
@@ -5,7 +5,51 @@ import IMN_DB as db
 import logging
 import time
 
-def IMN_read_station_webpage(station):
+def bs_get_table(soup_data, station, table_number = 1):
+    All_tables = soup_data.findAll("table")
+    table = All_tables[table_number - 1]
+    if table:
+        table_data=list()
+        for row in table.findAll("tr"):
+            row_data=list()
+            col_titles = row.findAll("th")
+            if col_titles:
+                for col in col_titles:
+                    row_data.append(col.contents[0])
+                table_data.append(row_data)
+                
+            col_data = row.findAll("td")
+            if col_data:
+                for col in col_data:
+                    row_data.append(str(col.contents[0]).replace(".", "").replace(",", "."))
+
+                table_data.append(row_data)
+        
+        if len(table_data):
+            if len(table_data[0]):
+                if table_data[0][0] == "Fecha":
+                    table_data[0] = [d.replace("PRES_mb", "P Atm") for d in table_data[0]]
+                    table_data[0] = [d.replace("Rad_PAR", "Rad PAR") for d in table_data[0]]
+                    table_data[0] = [d.replace("P.Atm", "P Atm") for d in table_data[0]]
+                    table_data[0] = [d.replace("P. Atm", "P Atm") for d in table_data[0]]
+                    table_data[0] = [d.replace("P Atm_Avg", "P Atm") for d in table_data[0]]
+                    table_data[0] = [d.replace("Nivel_Rio_Zapote", "Nivel") for d in table_data[0]]    
+                    data_df = pd.DataFrame(table_data[1:], columns=table_data[0])
+                    try:
+                        data_df["Fecha"] = pd.to_datetime(data_df["Fecha"], format="%d/%m/%Y %I:%M %p")
+                    except ValueError:
+                        try:
+                            data_df["Fecha"] = pd.to_datetime(data_df["Fecha"], format="%d/%m/%Y %H:%M:%S")
+                        except ValueError:
+                            logging.info(f'{station["Name"]}: Error parsing dates')
+                            return None
+                    cols = data_df.columns
+                    data_df[cols[1:]] = data_df[cols[1:]].apply(pd.to_numeric, errors='coerce')
+                    return data_df
+    return None
+
+
+def IMN_read_station_webpage(station, table_number = 1):
     #print("Station: ", station["Name"])
     response = None
     try:
@@ -17,45 +61,8 @@ def IMN_read_station_webpage(station):
     if response:
         if response.status_code == 200:
             soup_data = BeautifulSoup(response.text, 'html.parser')
-            table = soup_data.find("table")
-            if table:
-                table_data=list()
-                for row in table.findAll("tr"):
-                    row_data=list()
-                    col_titles = row.findAll("th")
-                    if col_titles:
-                        for col in col_titles:
-                            row_data.append(col.contents[0])
-                        table_data.append(row_data)
-                        
-                    col_data = row.findAll("td")
-                    if col_data:
-                        for col in col_data:
-                            row_data.append(str(col.contents[0]).replace(".", "").replace(",", "."))
-
-                        table_data.append(row_data)
-                
-                if len(table_data):
-                    if len(table_data[0]):
-                        if table_data[0][0] == "Fecha":
-                            table_data[0] = [d.replace("PRES_mb", "P Atm") for d in table_data[0]]
-                            table_data[0] = [d.replace("Rad_PAR", "Rad PAR") for d in table_data[0]]
-                            table_data[0] = [d.replace("P.Atm", "P Atm") for d in table_data[0]]
-                            table_data[0] = [d.replace("P. Atm", "P Atm") for d in table_data[0]]
-                            table_data[0] = [d.replace("P Atm_Avg", "P Atm") for d in table_data[0]]
-                            table_data[0] = [d.replace("Nivel_Rio_Zapote", "Nivel") for d in table_data[0]]    
-                            data_df = pd.DataFrame(table_data[1:], columns=table_data[0])
-                            try:
-                                data_df["Fecha"] = pd.to_datetime(data_df["Fecha"], format="%d/%m/%Y %I:%M %p")
-                            except ValueError:
-                                try:
-                                    data_df["Fecha"] = pd.to_datetime(data_df["Fecha"], format="%d/%m/%Y %H:%M:%S")
-                                except ValueError:
-                                    logging.info(f'{station["Name"]}: Error parsing dates')
-                                    return None
-                            cols = data_df.columns
-                            data_df[cols[1:]] = data_df[cols[1:]].apply(pd.to_numeric, errors='coerce')
-                            return data_df
+            return bs_get_table(soup_data, station, table_number)
+            
         else:
             logging.info(f'{station["Name"]}: Error cargando la pagina web')
             time.sleep(10)  ## wait to check for the next page
